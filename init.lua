0f434c65cfd0d0b163e74f712fcabbc1e5fd12b9
Nhanjk <tranchinhan20102021@gmail.com>
2024-05-26 19:47:53 +0700

    feat(init.lua): add mapping settings for neovim

--- init.lua
+++ init.lua
@@ -88,3 +88,28 @@ for _, provider in ipairs({ "node", "perl", "python3", "ruby" }) do
 end
 
 local is_windows = vim.fn.has("win32") ~= 0
+
+-- Key maps --------------------------------------------- 
+
+vim.g.mapleader = " "
+
+-- Normal mode mappings
+vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Window left' })
+vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Window right' })
+vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Window down' })
+vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Window up' })
+vim.keymap.set('n', '<C-Up>', '<cmd>resize -2<CR>', { desc = 'Resize up' })
+vim.keymap.set('n', '<C-Down>', '<cmd>resize +2<CR>', { desc = 'Resize down' })
+vim.keymap.set('n', '<C-Left>', '<cmd>vertical resize -2<CR>', { desc = 'Resize left' })
+vim.keymap.set('n', '<C-Right>', '<cmd>vertical resize +2<CR>', { desc = 'Resize right' })
+vim.keymap.set('n', '[b', '<cmd>bnext<cr>', { desc = 'Next Buffer' })
+vim.keymap.set('n', ']b', '<cmd>bprevious<cr>', { desc = 'Previous Buffer' })
+vim.keymap.set('n', '<S-Down>', function()
+  vim.api.nvim_feedkeys('5j', 'n', true)
+end, { desc = 'Fast move down' })
+vim.keymap.set('n', '<S-Up>', function()
+  vim.api.nvim_feedkeys('5k', 'n', true)
+end, { desc = 'Fast move up' })
+vim.keymap.set('n', '<C-s>', '<cmd> w <CR>', { desc = 'Save file' })
+vim.keymap.set('n', '<leader>|', '<cmd>vsplit<cr>', { desc = 'Vertical Split' })
+vim.keymap.set('n', '<leader>\\', '<cmd>split<cr>', { desc = 'Horizontal Split' })
