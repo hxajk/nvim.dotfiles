c6f185256e6f8d1a11b9ce7fc41ca52bd08ee187
Nhanjk <tranchinhan20102021@gmail.com>
2024-05-26 21:07:08 +0700

    feat(init.lua): add auto pairs.nvim to lazy.nvim

--- init.lua
+++ init.lua
@@ -807,4 +807,33 @@ require("lazy").setup({
         end,
     },
 
+     {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        lazy = true,
+        opts = {
+            check_ts = true,
+            ts_config = { java = false },
+            fast_wrap = {
+                map = "<M-e>",
+                chars = { "{", "[", "(", '"', "'" },
+                pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+                offset = 0,
+                end_key = "$",
+                keys = "qwertyuiopzxcvbnmasdfghjkl",
+                check_comma = true,
+                highlight = "PmenuSel",
+                highlight_grey = "LineNr",
+            },
+        },
+        config = function(_, opts)
+            require("nvim-autopairs").setup(opts)
+
+            require("cmp").event:on(
+                "confirm_done",
+                require("nvim-autopairs.completion.cmp").on_confirm_done({ tex = false })
+            )
+        end,
+    },
+
 })
