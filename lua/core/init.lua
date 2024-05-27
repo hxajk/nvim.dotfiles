063602530d22933ce6236bc10550cdeb7b5731fb
Nhanjk <tranchinhan20102021@gmail.com>
2024-05-27 16:30:42 +0700

    feat(core): add some useful lsp functions for the future

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -97,4 +97,43 @@ M.get_icon = function(kind, padding, enable) -- Later on...
     return icon and icon .. string.rep(" ", padding or 0) or ""
 end
 
+
+M.capabilities = function ()
+    require('cmp_nvim_lsp').default_capabilities()
+end
+
+M.on_attach = function(client, _)
+    
+    vim.keymap.set("n","<leader>lh",
+    function()
+        return vim.lsp.buf.signature_help
+    end,
+    {desc = "Show signature help"}
+    )
+    
+    vim.keymap.set("n", "<leader>li", "<cmd>LspInfo<cr>", {desc = "Lsp Info"})
+    
+    vim.keymap.set("n", "<leader>lk", 
+    function()
+        return vim.lsp.buf.hover 
+    end, 
+    {desc = "Hover"}
+    )
+
+    vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, {desc = "Code Action"})
+
+    vim.keymap.set("n", "<leader>lc", vim.lsp.codelens.run, {desc = "Run Codelens"})
+
+    vim.keymap.set("n", "<leader>lC", vim.lsp.codelens.refresh, {desc = "Refresh & Display Codelens"})
+    
+    	if client.supports_method("textDocument/formatting") then
+            vim.keymap.set("n", "<leader>lf", 
+            function() 
+                return vim.lsp.buf.format() 
+            end, 
+        {"Format buffer"}
+    )
+	end
+end
+
 return M
