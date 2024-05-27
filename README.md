b9d9ef343e4baed8aa8499977e033001ad9f87ef
Nhanjk <tranchinhan20102021@gmail.com>
2024-05-27 10:24:03 +0700

    feat(init.lua): add keymap for gitsigns.nvim

--- README.md
+++ README.md
@@ -2,6 +2,5 @@
 ⚡️ My personal neovim configuration that i 'll share with you guys
 
 # TODO 
-- Add keymap for gitsign 
 - Improve UI for which key
 - Add more keymap
--- init.lua
+++ init.lua
@@ -868,6 +868,22 @@ require("lazy").setup({
             local function map(mode, l, r, desc)
                 vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
             end
+            
+            -- stylua: ignore start
+        map("n", "]h", function() gs.nav_hunk("next") end, "Next Hunk")
+        map("n", "[h", function() gs.nav_hunk("prev") end, "Prev Hunk")
+        map("n", "]H", function() gs.nav_hunk("last") end, "Last Hunk")
+        map("n", "[H", function() gs.nav_hunk("first") end, "First Hunk")
+        map({ "n", "v" }, "<leader>gs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
+        map({ "n", "v" }, "<leader>gr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
+        map("n", "<leader>gS", gs.stage_buffer, "Stage Buffer")
+        map("n", "<leader>gu", gs.undo_stage_hunk, "Undo Stage Hunk")
+        map("n", "<leader>gR", gs.reset_buffer, "Reset Buffer")
+        map("n", "<leader>gp", gs.preview_hunk_inline, "Preview Hunk Inline")
+        map("n", "<leader>gb", function() gs.blame_line({ full = true }) end, "Blame Line")
+        map("n", "<leader>gd", gs.diffthis, "Diff This")
+        map("n", "<leader>gD", function() gs.diffthis("~") end, "Diff This ~")
+        map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
 
          end,
   },           
