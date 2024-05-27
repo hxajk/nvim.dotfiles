2a112370dd7baae2d41e7bbe4c239e8de6e5d8a5
Nhanjk <tranchinhan20102021@gmail.com>
2024-05-27 14:45:41 +0700

    feat(core): add some useful callback for the future

---
+++ lua/core/init.lua
@@ -0,0 +1,97 @@
+local M = {}
+
+
+M.notify = function(msg, type, opts)
+	vim.schedule(function()
+		vim.notify(msg, type, vim.tbl_deep_extend("force", { title = "3x3y3z" }, opts or {}))
+	end)
+end
+
+M.is_available = function(plugin)
+	local status, lazy = pcall(require, "lazy.core.config")
+	return status and lazy.spec.plugins[plugin] ~= nil
+end
+
+-- Icons
+
+M.Icons = {
+	-- AUTO COMPLETION --
+	kinds = {
+      Array         = " ",
+      Boolean       = "󰨙 ",
+      Class         = " ",
+      Codeium       = "󰘦 ",
+      Color         = " ",
+      Control       = " ",
+      Collapsed     = " ",
+      Constant      = "󰏿 ",
+      Constructor   = " ",
+      Copilot       = " ",
+      Enum          = " ",
+      EnumMember    = " ",
+      Event         = " ",
+      Field         = " ",
+      File          = " ",
+      Folder        = " ",
+      Function      = "󰊕 ",
+      Interface     = " ",
+      Key           = " ",
+      Keyword       = " ",
+      Method        = "󰊕 ",
+      Module        = " ",
+      Namespace     = "󰦮 ",
+      Null          = " ",
+      Number        = "󰎠 ",
+      Object        = " ",
+      Operator      = " ",
+      Package       = " ",
+      Property      = " ",
+      Reference     = " ",
+      Snippet       = " ",
+      String        = " ",
+      Struct        = "󰆼 ",
+      TabNine       = "󰏚 ",
+      Text          = " ",
+      TypeParameter = " ",
+      Unit          = " ",
+      Value         = " ",
+      Variable      = "󰀫 ",
+},
+	-- -- -- -- -- -- -- -- -- --
+
+	-- Base Icon --
+	base = {
+		DiagnosticError = "",
+		DiagnosticHint = "󰌵",
+		DiagnosticInfo = "󰋼",
+		DiagnosticWarn = "",
+		Git = "󰊢",
+        GitAdd = "▎",
+        GitChange = "▎",
+        GitDelete = "",
+        GitTopDelete = "",
+        GitChangeDelete = "▎",
+        GitUnTracked = "▎",
+		Session = "",
+		Tab = "󰓩",
+        Buffer = "󰓩", --Buffer == Tab, aight?
+		Terminal = "",
+		Message = "",
+        TelescopePrompt = "",
+        Flash = "⚡",
+        Clock = "",
+        Telescope = "", -- The same, but i want so...
+        LSP = "",
+	},
+
+	-- -- -- -- -- -- --
+}
+
+M.get_icon = function(kind, padding, enable) -- Later on...
+
+    local Base = M.Icons
+    local icon = Base["base"] and Base["base"][kind]
+    return icon and icon .. string.rep(" ", padding or 0) or ""
+end
+
+return M
