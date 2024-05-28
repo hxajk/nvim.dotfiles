47f9db9ec05610be1b40c1a2649e2bca0384ec54
Nhanjk <tranchinhan20102021@gmail.com>
2024-05-28 11:42:04 +0700

    feat(plugins): easier to control auto completion

--- lua/plugins/3-dev-tools.lua
+++ lua/plugins/3-dev-tools.lua
@@ -124,6 +124,7 @@ local default = {
 		"L3MON4D3/LuaSnip",
 		version = "v2.*",
 		event = "VeryLazy",
+		lazy = true,
 	},
 
 	{
@@ -168,10 +169,12 @@ local default = {
 					end,
 				},
 				mapping = {
-					["<C-d>"] = cmp.mapping.scroll_docs(-4),
+					["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+					["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
 					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.close(),
+					["<C-e>"] = cmp.mapping.abort(),
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
 					["<Tab>"] = function(fallback)
 						if cmp.visible() then
