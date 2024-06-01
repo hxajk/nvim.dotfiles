1335f4cf21680d79acce95326664f66141421b70
Nhanjk <tranchinhan20102021@gmail.com>
2024-06-01 10:26:45 +0700

    feat(plugins): add new hightlight for auto completion

--- lua/plugins/3-dev-tools.lua
+++ lua/plugins/3-dev-tools.lua
@@ -226,6 +226,25 @@ local default = {
 
 		config = function(_, opts)
 			require("cmp").setup(opts)
+
+			-- From cmp wiki (https://github.com/hrsh7th/nvim-cmp/wiki/Menu-Appearance)
+			-- gray
+			vim.api.nvim_set_hl(0, "CmpItemAbbrDeprecated", { bg = "NONE", strikethrough = true, fg = "#808080" })
+			-- blue
+			vim.api.nvim_set_hl(0, "CmpItemAbbrMatch", { bg = "NONE", fg = "#569CD6" })
+			vim.api.nvim_set_hl(0, "CmpItemAbbrMatchFuzzy", { link = "CmpIntemAbbrMatch" })
+			-- light blue
+			vim.api.nvim_set_hl(0, "CmpItemKindVariable", { bg = "NONE", fg = "#9CDCFE" })
+			vim.api.nvim_set_hl(0, "CmpItemKindInterface", { link = "CmpItemKindVariable" })
+			vim.api.nvim_set_hl(0, "CmpItemKindText", { link = "CmpItemKindVariable" })
+			-- pink
+			vim.api.nvim_set_hl(0, "CmpItemKindFunction", { bg = "NONE", fg = "#C586C0" })
+			vim.api.nvim_set_hl(0, "CmpItemKindMethod", { link = "CmpItemKindFunction" })
+			-- front
+			vim.api.nvim_set_hl(0, "CmpItemKindKeyword", { bg = "NONE", fg = "#D4D4D4" })
+			vim.api.nvim_set_hl(0, "CmpItemKindProperty", { link = "CmpItemKindKeyword" })
+			vim.api.nvim_set_hl(0, "CmpItemKindUnit", { link = "CmpItemKindKeyword" })
+
 		end,
 	},
 
