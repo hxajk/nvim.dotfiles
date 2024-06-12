e23ac586784e29e57c695ca43c8c05ba52796399
Nhanjk <tranchinhan20102021@gmail.com>
2024-06-12 13:23:50 +0700

    refactor(plugins): improve lualine status UI

--- lua/plugins/2-UI.lua
+++ lua/plugins/2-UI.lua
@@ -264,22 +264,51 @@ local default = {
 			end
 		end,
 
-		opts = function()
-			return {
-				options = {
-					theme = require("core").theme(),
-					globalstatus = true,
-					disabled_filetypes = { statusline = { "dashboard", "alpha", "starter" } },
-				},
-				sections = {
-					lualine_z = {
-						function()
-							return " " .. os.date("%R")
-						end,
-					},
-				},
-			}
-		end,
+	
+opts = function()
+    return {
+        options = {
+            theme = require("core").theme(),
+            component_separators = "",
+            disabled_filetypes = { statusline = { "dashboard", "alpha", "starter" } },
+            section_separators = { left = " ", right = " " },
+        },
+        sections = {
+            lualine_a = { "mode" },
+            lualine_b = {
+                { "branch" },
+                {
+                    "diff",
+                    symbols = { 
+                        added = "  ", 
+                        modified = "  ", 
+                        removed = " " 
+                    }, 
+                },
+                { "diagnostics" },
+            },
+            lualine_c = {
+                { 
+                    "filetype", 
+                    icon_only = true, 
+                    padding = { left = 1, right = 0 }, 
+                    separator = " " 
+                },
+                { 
+                    "filename", 
+                    padding = { left = 0, right = 1 } 
+                },
+            },
+            lualine_x = {},
+            lualine_y = { "progress" },
+            lualine_z = {
+                function()
+                    return " " .. os.date("%R")
+                end,
+            },
+        },
+    }
+end,
 
 		config = function(_, opts)
 			require("lualine").setup(opts)
