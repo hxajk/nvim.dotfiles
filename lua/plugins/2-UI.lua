e72997cf0ae477a1da3e4084130c04f1233524d8
Nhanjk <tranchinhan20102021@gmail.com>
2024-06-12 12:41:54 +0700

    refactor(plugins): improve noice.nvim UI

--- lua/plugins/2-UI.lua
+++ lua/plugins/2-UI.lua
@@ -196,11 +196,13 @@ local default = {
                         local hint = #vim.diagnostic.get(0, { severity = seve.HINT })
 
                         if error ~= 0 then
-                            table.insert(result, { text = get_icons("DiagnosticError", 1, true) .. error, fg = "#EC5241" })
+                            table.insert(result,
+                                { text = get_icons("DiagnosticError", 1, true) .. error, fg = "#EC5241" })
                         end
 
                         if warning ~= 0 then
-                            table.insert(result, { text = get_icons("DiagnosticWarn", 1, true) .. warning, fg = "#EFB839" })
+                            table.insert(result,
+                                { text = get_icons("DiagnosticWarn", 1, true) .. warning, fg = "#EFB839" })
                         end
 
                         if hint ~= 0 then
@@ -332,31 +334,35 @@ local default = {
             { "MunifTanjim/nui.nvim", lazy = true },
         },
         opts = {
-            lsp = {
-                override = {
-                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                    ["vim.lsp.util.stylize_markdown"] = true,
-                    ["cmp.entry.get_documentation"] = true,
-                },
-            },
-            routes = {
-                {
-                    filter = {
-                        event = "msg_show",
-                        any = {
-                            { find = "%d+L, %d+B" },
-                            { find = "; after #%d+" },
-                            { find = "; before #%d+" },
-                        },
+            views = {
+                cmdline_popup = {
+                    position = {
+                        row = 5,
+                        col = "50%",
                     },
-                    view = "mini",
+                    size = {
+                        width = 60,
+                        height = "auto",
+                    },
                 },
-            },
-            presets = {
-                bottom_search = true,
-                command_palette = true,
-                long_message_to_split = true,
-                inc_rename = true,
+                popupmenu = {
+                    relative = "editor",
+                    position = {
+                        row = 8,
+                        col = "50%",
+                    },
+                    size = {
+                        width = 60,
+                        height = 10,
+                    },
+                    border = {
+                        style = "rounded",
+                        padding = { 0, 1 },
+                    },
+                    win_options = {
+                        winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+                    },
+                },
             },
         },
 
