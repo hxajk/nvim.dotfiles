20c6c80423aa897c455522489170ac8972941778
Nhanjk <tranchinhan20102021@gmail.com>
2024-06-13 15:19:31 +0700

    style(plugins): formatting

--- lua/plugins/3-dev-tools.lua
+++ lua/plugins/3-dev-tools.lua
@@ -3,292 +3,292 @@
 local get_icons = require("core").get_icon
 
 local default = {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        version = false,
-        event = "BufReadPre",
-        build = function()
-            if #vim.api.nvim_list_uis() ~= 0 then
-                vim.api.nvim_command([[TSUpdate]])
-            end
-        end,
-        lazy = vim.fn.argc(-1) == 0,
-        init = function(plugin)
-            require("lazy.core.loader").add_to_rtp(plugin)
-            require("nvim-treesitter.query_predicates")
-        end,
-        cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-        opts = {
-            auto_install = vim.fn.executable("git") == 1 and vim.fn.executable("tree-sitter") == 1,
-            hightlight = {
-                enable = true,
-                disable = { "help" },
-            },
-            indent = { enable = true },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		version = false,
+		event = { "BufRead", "BufNewFile" },
+		build = function()
+			if #vim.api.nvim_list_uis() ~= 0 then
+				vim.api.nvim_command([[TSUpdate]])
+			end
+		end,
+		lazy = vim.fn.argc(-1) == 0,
+		init = function(plugin)
+			require("lazy.core.loader").add_to_rtp(plugin)
+			require("nvim-treesitter.query_predicates")
+		end,
+		cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+		opts = {
+			auto_install = vim.fn.executable("git") == 1 and vim.fn.executable("tree-sitter") == 1,
+			hightlight = {
+				enable = true,
+				disable = { "help" },
+			},
+			indent = { enable = true },
 
-            ensure_installed = {
-                "markdown",
-                "vim",
-                "lua",
-            },
-        },
-        config = function(_, opts)
-            require("nvim-treesitter.configs").setup(opts)
-        end,
-    },
+			ensure_installed = {
+				"markdown",
+				"vim",
+				"lua",
+			},
+		},
+		config = function(_, opts)
+			require("nvim-treesitter.configs").setup(opts)
+		end,
+	},
 
-    {
-        -- LSP and Autocompletion
-        "neovim/nvim-lspconfig",
-        lazy = true,
-        event = { "CursorHold", "CursorHoldI" },
-        opts = function()
-            return {
-                underline = true,
-                severity_sort = true,
-                update_in_insert = false,
-                diagnostic = {
-                    underline = true,
-                    update_in_insert = false,
-                    virtual_text = {
-                        spacing = 4,
-                        source = "if_many",
-                        prefix = "●",
-                    },
-                    severity_sort = true,
-                },
-            }
-        end,
-        config = function(_, opts)
-            vim.diagnostic.config(vim.deepcopy(opts.diagnostics))
+	{
+		-- LSP and Autocompletion
+		"neovim/nvim-lspconfig",
+		lazy = true,
+		event = { "CursorHold", "CursorHoldI" },
+		opts = function()
+			return {
+				underline = true,
+				severity_sort = true,
+				update_in_insert = false,
+				diagnostic = {
+					underline = true,
+					update_in_insert = false,
+					virtual_text = {
+						spacing = 4,
+						source = "if_many",
+						prefix = "●",
+					},
+					severity_sort = true,
+				},
+			}
+		end,
+		config = function(_, opts)
+			vim.diagnostic.config(vim.deepcopy(opts.diagnostics))
 
-            local servers = { "clangd", "pyright" }
+			local servers = { "clangd", "pyright" }
 
-            for _, server in ipairs(servers) do
-                require("lspconfig")[server].setup({
-                    on_attach = function(client, buffer)
-                        require("core").on_attach(client, buffer)
-                    end,
+			for _, server in ipairs(servers) do
+				require("lspconfig")[server].setup({
+					on_attach = function(client, buffer)
+						require("core").on_attach(client, buffer)
+					end,
 
-                    capabilities = require("core").capabilities(),
-                })
-            end
+					capabilities = require("core").capabilities(),
+				})
+			end
 
-            require("lspconfig").lua_ls.setup({
-                on_attach = function(client, buffer)
-                    require("core").on_attach(client, buffer)
-                end,
+			require("lspconfig").lua_ls.setup({
+				on_attach = function(client, buffer)
+					require("core").on_attach(client, buffer)
+				end,
 
-                capabilities = require("cmp_nvim_lsp").default_capabilities(),
+				capabilities = require("cmp_nvim_lsp").default_capabilities(),
 
-                settings = {
-                    Lua = {
-                        diagnostics = {
-                            globals = { "vim" },
-                        },
-                    },
-                },
-            })
-            vim.fn.sign_define(
-                "DiagnosticSignError",
-                { text = get_icons("DiagnosticError", 1, true), texthl = "DiagnosticSignError" }
-            )
-            vim.fn.sign_define(
-                "DiagnosticSignWarn",
-                { text = get_icons("DiagnosticWarn", 1, true), texthl = "DiagnosticSignWarn" }
-            )
-            vim.fn.sign_define(
-                "DiagnosticSignInfo",
-                { text = get_icons("DiagnosticInfo", 1, true), texthl = "DiagnosticSignInfo" }
-            )
-            vim.fn.sign_define(
-                "DiagnosticSignHint",
-                { text = get_icons("DiagnosticHint", 1, true), texthl = "DiagnosticSignHint" }
-            )
-        end,
-    },
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" },
+						},
+					},
+				},
+			})
+			vim.fn.sign_define(
+				"DiagnosticSignError",
+				{ text = get_icons("DiagnosticError", 1, true), texthl = "DiagnosticSignError" }
+			)
+			vim.fn.sign_define(
+				"DiagnosticSignWarn",
+				{ text = get_icons("DiagnosticWarn", 1, true), texthl = "DiagnosticSignWarn" }
+			)
+			vim.fn.sign_define(
+				"DiagnosticSignInfo",
+				{ text = get_icons("DiagnosticInfo", 1, true), texthl = "DiagnosticSignInfo" }
+			)
+			vim.fn.sign_define(
+				"DiagnosticSignHint",
+				{ text = get_icons("DiagnosticHint", 1, true), texthl = "DiagnosticSignHint" }
+			)
+		end,
+	},
 
-    {
-        "stevearc/conform.nvim",
-        lazy = true,
-        cmd = "ConformInfo",
-        event = { "CursorHold", "CursorHoldI" },
-        opts = {
-            formatters_by_ft = {
-                lua = { "stylua" },
-            },
-        },
-        config = function(_, opts)
-            require("conform").setup(opts)
-            vim.keymap.set("n", "<leader>l", "<leader>l", { desc = "+" .. get_icons("LSP", 1, true) .. "LSP" })
-            vim.keymap.set("n", "<leader>lu", function()
-                vim.g.autoformat = not vim.g.autoformat
+	{
+		"stevearc/conform.nvim",
+		lazy = true,
+		cmd = "ConformInfo",
+		event = { "CursorHold", "CursorHoldI" },
+		opts = {
+			formatters_by_ft = {
+				lua = { "stylua" },
+			},
+		},
+		config = function(_, opts)
+			require("conform").setup(opts)
+			vim.keymap.set("n", "<leader>l", "<leader>l", { desc = "+" .. get_icons("LSP", 1, true) .. "LSP" })
+			vim.keymap.set("n", "<leader>lu", function()
+				vim.g.autoformat = not vim.g.autoformat
 
-                if vim.g.autoformat then
-                    vim.notify("Enable Auto Format")
-                else
-                    vim.notify("Disable Auto Format")
-                end
-            end, { desc = "Toggle autoformat" })
+				if vim.g.autoformat then
+					vim.notify("Enable Auto Format")
+				else
+					vim.notify("Disable Auto Format")
+				end
+			end, { desc = "Toggle autoformat" })
 
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                pattern = "*",
-                callback = function(args)
-                    if vim.g.autoformat then
-                        require("conform").format({ bufnr = args.buf })
-                    end
-                end,
-            })
-        end,
-    },
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				pattern = "*",
+				callback = function(args)
+					if vim.g.autoformat then
+						require("conform").format({ bufnr = args.buf })
+					end
+				end,
+			})
+		end,
+	},
 
-    {
-        "L3MON4D3/LuaSnip",
-        version = "v2.*",
-        event = "VeryLazy",
-        lazy = true,
-    },
+	{
+		"L3MON4D3/LuaSnip",
+		version = "v2.*",
+		event = "VeryLazy",
+		lazy = true,
+	},
 
-    {
-        "hrsh7th/nvim-cmp",
-        dependencies = {
-            "hrsh7th/cmp-nvim-lsp",
-        },
-        lazy = true,
-        event = "InsertEnter",
-        version = false,
-        opts = function()
-            local cmp = require("cmp")
-            local luasnip = require("luasnip")
-            local icons = require("core").Icons.kinds
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+		},
+		lazy = true,
+		event = "InsertEnter",
+		version = false,
+		opts = function()
+			local cmp = require("cmp")
+			local luasnip = require("luasnip")
+			local icons = require("core").Icons.kinds
 
-            return {
-                snippet = {
-                    expand = function(args)
-                        require("luasnip").lsp_expand(args.body)
-                    end,
-                },
+			return {
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
 
-                window = {
-                    completion = cmp.config.window.bordered(),
-                    documentation = cmp.config.window.bordered(),
-                },
-                experimental = {
-                    ghost_text = {
-                        hl_group = "CmpGhostText",
-                    },
-                },
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+				experimental = {
+					ghost_text = {
+						hl_group = "CmpGhostText",
+					},
+				},
 
-                formatting = {
-                    fields = { "abbr", "kind", "menu" },
-                    format = function(_, item)
-                        -- Kind icons
-                        if icons[item.kind] then
-                            item.kind = icons[item.kind] .. " " .. item.kind
-                        end
+				formatting = {
+					fields = { "abbr", "kind", "menu" },
+					format = function(_, item)
+						-- Kind icons
+						if icons[item.kind] then
+							item.kind = icons[item.kind] .. " " .. item.kind
+						end
 
-                        return item
-                    end,
-                },
-                mapping = {
-                    ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-                    ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                    ["<C-e>"] = cmp.mapping.abort(),
-                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-                    ["<Tab>"] = function(fallback)
-                        if cmp.visible() then
-                            cmp.select_next_item()
-                        elseif luasnip.expand_or_jumpable() then
-                            luasnip.expand_or_jump()
-                        else
-                            fallback()
-                        end
-                    end,
-                    ["<S-Tab>"] = function(fallback)
-                        if cmp.visible() then
-                            cmp.select_prev_item()
-                        elseif luasnip.jumpable(-1) then
-                            luasnip.jump(-1)
-                        else
-                            fallback()
-                        end
-                    end,
-                },
-                sources = {
-                    { name = "nvim_lsp" },
-                    { name = "luasnip" },
-                },
-            }
-        end,
+						return item
+					end,
+				},
+				mapping = {
+					["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+					["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					["<Tab>"] = function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						else
+							fallback()
+						end
+					end,
+					["<S-Tab>"] = function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end,
+				},
+				sources = {
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+				},
+			}
+		end,
 
-        config = function(_, opts)
-            require("cmp").setup(opts)
+		config = function(_, opts)
+			require("cmp").setup(opts)
 
-            -- From cmp wiki (https://github.com/hrsh7th/nvim-cmp/wiki/Menu-Appearance)
-            -- gray
-            vim.api.nvim_set_hl(0, "CmpItemAbbrDeprecated", { bg = "NONE", strikethrough = true, fg = "#808080" })
-            -- blue
-            vim.api.nvim_set_hl(0, "CmpItemAbbrMatch", { bg = "NONE", fg = "#569CD6" })
-            vim.api.nvim_set_hl(0, "CmpItemAbbrMatchFuzzy", { link = "CmpIntemAbbrMatch" })
-            -- light blue
-            vim.api.nvim_set_hl(0, "CmpItemKindVariable", { bg = "NONE", fg = "#9CDCFE" })
-            vim.api.nvim_set_hl(0, "CmpItemKindInterface", { link = "CmpItemKindVariable" })
-            vim.api.nvim_set_hl(0, "CmpItemKindText", { link = "CmpItemKindVariable" })
-            -- pink
-            vim.api.nvim_set_hl(0, "CmpItemKindFunction", { bg = "NONE", fg = "#C586C0" })
-            vim.api.nvim_set_hl(0, "CmpItemKindMethod", { link = "CmpItemKindFunction" })
-            -- front
-            vim.api.nvim_set_hl(0, "CmpItemKindKeyword", { bg = "NONE", fg = "#D4D4D4" })
-            vim.api.nvim_set_hl(0, "CmpItemKindProperty", { link = "CmpItemKindKeyword" })
-            vim.api.nvim_set_hl(0, "CmpItemKindUnit", { link = "CmpItemKindKeyword" })
-        end,
-    },
+			-- From cmp wiki (https://github.com/hrsh7th/nvim-cmp/wiki/Menu-Appearance)
+			-- gray
+			vim.api.nvim_set_hl(0, "CmpItemAbbrDeprecated", { bg = "NONE", strikethrough = true, fg = "#808080" })
+			-- blue
+			vim.api.nvim_set_hl(0, "CmpItemAbbrMatch", { bg = "NONE", fg = "#569CD6" })
+			vim.api.nvim_set_hl(0, "CmpItemAbbrMatchFuzzy", { link = "CmpIntemAbbrMatch" })
+			-- light blue
+			vim.api.nvim_set_hl(0, "CmpItemKindVariable", { bg = "NONE", fg = "#9CDCFE" })
+			vim.api.nvim_set_hl(0, "CmpItemKindInterface", { link = "CmpItemKindVariable" })
+			vim.api.nvim_set_hl(0, "CmpItemKindText", { link = "CmpItemKindVariable" })
+			-- pink
+			vim.api.nvim_set_hl(0, "CmpItemKindFunction", { bg = "NONE", fg = "#C586C0" })
+			vim.api.nvim_set_hl(0, "CmpItemKindMethod", { link = "CmpItemKindFunction" })
+			-- front
+			vim.api.nvim_set_hl(0, "CmpItemKindKeyword", { bg = "NONE", fg = "#D4D4D4" })
+			vim.api.nvim_set_hl(0, "CmpItemKindProperty", { link = "CmpItemKindKeyword" })
+			vim.api.nvim_set_hl(0, "CmpItemKindUnit", { link = "CmpItemKindKeyword" })
+		end,
+	},
 
-    {
-        "williamboman/mason.nvim",
-        cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUpdate" },
-        build = ":MasonUpdate",
-        lazy = true,
-        opts = {
-            ui = {
-                check_outdated_packages_on_open = true,
-                width = 0.8,
-                height = 0.9,
-                border = "rounded",
-            },
-            log_level = vim.log.levels.INFO,
-        },
-        keys = {
-            { "<leader>p",  "<Cmd><leader>p<CR>",   desc = "+" .. get_icons("Package", 1, true) .. "Packages" },
-            { "<leader>pm", "<Cmd>Mason<CR>",       desc = "Open Language Menu" },
-            { "<leader>pu", "<Cmd>MasonUpdate<CR>", desc = "Refesh Language" },
-        },
-        config = function(_, opts)
-            local ensure_installed = { "lua-language-server", "stylua" }
+	{
+		"williamboman/mason.nvim",
+		cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUpdate" },
+		build = ":MasonUpdate",
+		lazy = true,
+		opts = {
+			ui = {
+				check_outdated_packages_on_open = true,
+				width = 0.8,
+				height = 0.9,
+				border = "rounded",
+			},
+			log_level = vim.log.levels.INFO,
+		},
+		keys = {
+			{ "<leader>p", "<Cmd><leader>p<CR>", desc = "+" .. get_icons("Package", 1, true) .. "Packages" },
+			{ "<leader>pm", "<Cmd>Mason<CR>", desc = "Open Language Menu" },
+			{ "<leader>pu", "<Cmd>MasonUpdate<CR>", desc = "Refesh Language" },
+		},
+		config = function(_, opts)
+			local ensure_installed = { "lua-language-server", "stylua" }
 
-            local mr = require("mason-registry")
+			local mr = require("mason-registry")
 
-            require("mason").setup(opts)
+			require("mason").setup(opts)
 
-            local function installer()
-                for _, tool in ipairs(ensure_installed) do
-                    local p = mr.get_package(tool)
-                    if not p:is_installed() then
-                        p:install()
-                    end
-                end
-            end
+			local function installer()
+				for _, tool in ipairs(ensure_installed) do
+					local p = mr.get_package(tool)
+					if not p:is_installed() then
+						p:install()
+					end
+				end
+			end
 
-            if mr.refresh then
-                mr.refresh(installer)
-            else
-                installer()
-            end
-        end,
-    },
+			if mr.refresh then
+				mr.refresh(installer)
+			else
+				installer()
+			end
+		end,
+	},
 }
 
 return default
