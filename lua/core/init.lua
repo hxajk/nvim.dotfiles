2b58b47dbbb68c8f85f5d4896171da46c62d2897
Nhanjk <tranchinhan20102021@gmail.com>
2024-06-16 14:10:46 +0700

    style(core): formatting

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -3,84 +3,84 @@
 local M = {}
 
 M.notify = function(msg, type, opts)
-    vim.schedule(function()
-        vim.notify(msg, type, vim.tbl_deep_extend("force", { title = "3x3y3z" }, opts or {}))
-    end)
+	vim.schedule(function()
+		vim.notify(msg, type, vim.tbl_deep_extend("force", { title = "3x3y3z" }, opts or {}))
+	end)
 end
 
 M.is_available = function(plugin)
-    local status, lazy = pcall(require, "lazy.core.config")
-    return status and lazy.spec.plugins[plugin] ~= nil
+	local status, lazy = pcall(require, "lazy.core.config")
+	return status and lazy.spec.plugins[plugin] ~= nil
 end
 
 M.create_file = function()
-    local extension = vim.fn.input("Enter your file type e.g (.txt, .lua, ...): ")
+	local extension = vim.fn.input("Enter your file type e.g (.txt, .lua, ...): ")
 
-    if extension == "" then
-        print("No extension provided, operation cancelled.")
-        return
-    end
+	if extension == "" then
+		print("No extension provided, operation cancelled.")
+		return
+	end
 
-    local filename = "newfile" .. extension
-    vim.cmd("e " .. filename)
+	local filename = "newfile" .. extension
+	vim.cmd("e " .. filename)
 
-    local filetype = vim.fn.fnamemodify(filename, ":e")
-    vim.cmd("setlocal filetype=" .. filetype)
+	local filetype = vim.fn.fnamemodify(filename, ":e")
+	vim.cmd("setlocal filetype=" .. filetype)
 
-    print("Created file: " .. filename .. " with filetype: " .. filetype)
+	print("Created file: " .. filename .. " with filetype: " .. filetype)
 end
 
 -- lualine
 
 M.lsp_progress = function()
-    local cur = vim.fn.line(".")
-    local total = vim.fn.line("$")
-    local spinners = { "", "", "" }
+	local cur = vim.fn.line(".")
+	local total = vim.fn.line("$")
+	local spinners = { "", "", "" }
 
-    if cur == 1 then
-        return spinners[1]
-    elseif cur == total then
-        return spinners[3]
-    else
-        local ms = vim.loop.hrtime() / 1000000
-        local frame = math.floor(ms / 120) % #spinners
+	if cur == 1 then
+		return spinners[1]
+	elseif cur == total then
+		return spinners[3]
+	else
+		local ms = vim.loop.hrtime() / 1000000
+		local frame = math.floor(ms / 120) % #spinners
 
-        return string.format(" %%<%s %s%%%% ", spinners[frame + 1], math.floor(cur / total * 100))
-    end
+		return string.format(" %%<%s %s%%%% ", spinners[frame + 1], math.floor(cur / total * 100))
+	end
 end
 
 M.theme = function()
-    -- -- Define the gruvbox color pallete
-    -- local colors = {
-    -- 	black = "#282828",
-    -- 	white = "#ebdbb2",
-    -- 	red = "#fb4934",
-    -- 	green = "#b8bb26",
-    -- 	blue = "#83a598",
-    -- 	yellow = "#fe8019",
-    -- 	gray = "#a89984",
-    -- 	darkgray = "#3c3836",
-    -- 	lightgray = "#504945",
-    -- 	inactivegray = "#7c6f64",
-    -- 	outerbg = "None",
-    -- 	innerbg = "None",
-    -- }
+	-- -- Define the gruvbox color pallete
+	-- local colors = {
+	-- 	black = "#282828",
+	-- 	white = "#ebdbb2",
+	-- 	red = "#fb4934",
+	-- 	green = "#b8bb26",
+	-- 	blue = "#83a598",
+	-- 	yellow = "#fe8019",
+	-- 	gray = "#a89984",
+	-- 	darkgray = "#3c3836",
+	-- 	lightgray = "#504945",
+	-- 	inactivegray = "#7c6f64",
+	-- 	outerbg = "None",
+	-- 	innerbg = "None",
+	-- }
 
-    -- Define the evil lualine color palette
-    local colors = {
-        bg       = '#202328',
-        fg       = '#bbc2cf',
-    }
-    return {
-        normal = { c = { fg = colors.fg, bg = colors.bg } },
-        inactive = { c = { fg = colors.fg, bg = colors.bg } },
-    }
+	-- Define the evil lualine color palette
+	local colors = {
+		bg = "#202328",
+		fg = "#bbc2cf",
+	}
+	return {
+		normal = { c = { fg = colors.fg, bg = colors.bg } },
+		inactive = { c = { fg = colors.fg, bg = colors.bg } },
+	}
 end
 -- Icons
 
 local icons = {
-    -- AUTO COMPLETION --
-    kind = {
+	-- AUTO COMPLETION --
+	kind = {
 		Class = "󰠱",
 		Color = "󰏘",
 		Constant = "󰏿",
@@ -117,11 +117,11 @@ local icons = {
 		Parameter = "",
 		StaticMethod = "",
 		Macro = "",
-	},    -- -- -- -- -- -- -- -- -- --
+	}, -- -- -- -- -- -- -- -- -- --
 
-    -- Base Icon --
+	-- Base Icon --
 
-    type = {
+	type = {
 		Array = "󰅪",
 		Boolean = "",
 		Null = "󰟢",
@@ -130,7 +130,7 @@ local icons = {
 		String = "󰉿",
 	},
 
-    git = {
+	git = {
 		Add = "",
 		Branch = "",
 		Diff = "",
@@ -148,7 +148,7 @@ local icons = {
 		Conflict = "",
 	},
 
-    ui = {
+	ui = {
 		Accepted = "",
 		ArrowClosed = "",
 		ArrowOpen = "",
@@ -211,7 +211,7 @@ local icons = {
 		Table = "",
 		Telescope = "",
 		Window = "",
-        Message = "",
+		Message = "",
 	},
 	diagnostics = {
 		Error = "",
@@ -246,78 +246,78 @@ local icons = {
 		ManUp = "",
 		Neovim = "",
 		Vim = "",
-        Package = "",
-        LSP = "",
+		Package = "",
+		LSP = "",
 	},
 
-    tools = {
-        Session = "",
-        Terminal = "",
-        Telescope = "",
-        TelescopePrompt = "",
-        Selected = "❯",
-    },
+	tools = {
+		Session = "",
+		Terminal = "",
+		Telescope = "",
+		TelescopePrompt = "",
+		Selected = "❯",
+	},
 
-    base = {
-        Buffer = "󰓩",
-        Clock = "",
-        DiagnosticError = "",
-        DiagnosticHint = "󰌵",
-        DiagnosticInfo = "󰋼",
-        DiagnosticWarn = "",
-        Flash = "⚡",
-        Git = "󰊢",
-        GitAdd = "▎",
-        GitChange = "▎",
-        GitChangeDelete = "▎",
-        GitDelete = "",
-        GitTopDelete = "",
-        GitUnTracked = "▎",
-        Tab = "󰓩",
-        VBar = "│",
-    },
+	base = {
+		Buffer = "󰓩",
+		Clock = "",
+		DiagnosticError = "",
+		DiagnosticHint = "󰌵",
+		DiagnosticInfo = "󰋼",
+		DiagnosticWarn = "",
+		Flash = "⚡",
+		Git = "󰊢",
+		GitAdd = "▎",
+		GitChange = "▎",
+		GitChangeDelete = "▎",
+		GitDelete = "",
+		GitTopDelete = "",
+		GitUnTracked = "▎",
+		Tab = "󰓩",
+		VBar = "│",
+	},
 
-    -- -- -- -- -- -- --
+	-- -- -- -- -- -- --
 }
 
 M.gets = function(category, add_space)
-    if not vim.g.icons_enabled then
-        return ""
-    end
-		return setmetatable({}, {
-			__index = function(_, key)
-				return icons[category][key] .. " "
-			end,
-		})
+	if not vim.g.icons_enabled then
+		return ""
+	end
+	return setmetatable({}, {
+		__index = function(_, key)
+			return icons[category][key] .. " "
+		end,
+	})
 end
 
 M.capabilities = function()
-    require("cmp_nvim_lsp").default_capabilities()
+	require("cmp_nvim_lsp").default_capabilities()
 end
 
 M.on_attach = function(client, bufnr)
-    local function map(mode, key, core, desc)
-        vim.keymap.set(mode, key, core, { desc = desc, buffer = bufnr })
-    end
+	local function map(mode, key, core, desc)
+		vim.keymap.set(mode, key, core, { desc = desc, buffer = bufnr })
+	end
 
-    -- Using the custom map function
+	-- Using the custom map function
 
-    map("n", "<leader>l", "<leader>l", M.gets("base").LSP .. "LSP")
+	map("n", "<leader>l", "<leader>l", M.gets("base").LSP .. "LSP")
 
-    map("n", "<leader>lh", vim.lsp.buf.signature_help, "Show signature help")
+	map("n", "<leader>lh", vim.lsp.buf.signature_help, "Show signature help")
 
-    map("n", "<leader>li", "<cmd>LspInfo<cr>", "Lsp Info")
+	map("n", "<leader>li", "<cmd>LspInfo<cr>", "Lsp Info")
 
-    map("n", "<leader>lk", vim.lsp.buf.hover, "Hover")
+	map("n", "<leader>lk", vim.lsp.buf.hover, "Hover")
 
-    map("n", "[d", vim.diagnostic.goto_prev, "Previous Diagnostic")
+	map("n", "[d", vim.diagnostic.goto_prev, "Previous Diagnostic")
 
-    map("n", "]d", vim.diagnostic.goto_next, "Next Diagnostic")
+	map("n", "]d", vim.diagnostic.goto_next, "Next Diagnostic")
 
-    map("n", "<leader>ln", vim.lsp.buf.rename, "Rename Varable")
+	map("n", "<leader>ln", vim.lsp.buf.rename, "Rename Varable")
 
-    if client.supports_method("textDocument/formatting") then
-        map({ "n", "v" }, "<leader>lf", vim.lsp.buf.format, "Format buffer")
-    end
+	if client.supports_method("textDocument/formatting") then
+		map({ "n", "v" }, "<leader>lf", vim.lsp.buf.format, "Format buffer")
+	end
 end
 return M
