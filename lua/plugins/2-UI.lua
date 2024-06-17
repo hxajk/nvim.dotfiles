b9f32b024afa1c5179390963ec9597af4170c17e
Nhanjk <tranchinhan20102021@gmail.com>
2024-06-17 08:01:12 +0700

    feat(plugins): add nvim-scrollview plugin for UI plugins

--- lua/plugins/2-UI.lua
+++ lua/plugins/2-UI.lua
@@ -416,6 +416,25 @@ local default = {
 		end,
 	},
 
+	-- nvim-scrollview -> [Displays interactive vertical scrollbars and signs]
+	-- https://github.com/dstein64/nvim-scrollview
+
+	{
+		"dstein64/nvim-scrollview",
+		config = function()
+			require("scrollview").setup({
+				mode = "virtual",
+				excluded_filetypes = { "NvimTree", "terminal", "nofile", "aerial" },
+				winblend = 0,
+				signs_on_startup = { "diagnostics", "folds", "marks", "search", "spell" },
+				diagnostics_error_symbol = icons.diagnostics.Error,
+				diagnostics_warn_symbol = icons.diagnostics.Warning,
+				diagnostics_info_symbol = icons.diagnostics.Information,
+				diagnostics_hint_symbol = icons.diagnostics.Hint,
+			})
+		end,
+	},
+
 	{
 		"folke/which-key.nvim",
 		lazy = true,
