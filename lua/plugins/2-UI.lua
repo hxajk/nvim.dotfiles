8b33c3136cf41978066001c8a037493afa0d57e5
Nhanjk <tranchinhan20102021@gmail.com>
2024-06-18 09:39:42 +0700

    refactor(plugins): improve buffer UI

--- lua/plugins/2-UI.lua
+++ lua/plugins/2-UI.lua
@@ -184,12 +184,26 @@ local default = {
 		event = { "BufReadPost", "BufAdd", "BufNewFile" },
 		opts = {
 			options = {
-				themable = true,
-				diagnostics = "nvim_lsp",
-				diagnostics_update_in_insert = false,
-				diagnostics_indicator = function(count)
-					return "(" .. count .. ")"
-				end,
+			number = nil,
+			buffer_close_icon = icons.ui.Close,
+			left_trunc_marker = icons.ui.Left,
+			right_trunc_marker = icons.ui.Right,
+			max_name_length = 20,
+			max_prefix_length = 13,
+			tab_size = 20,
+			color_icons = true,
+			show_buffer_icons = true,
+			show_buffer_close_icons = true,
+			show_close_icon = true,
+			show_tab_indicators = true,
+			enforce_regular_tabs = false,
+			persist_buffer_sort = true,
+			always_show_bufferline = true,
+			separator_style = "thin",
+			diagnostics = "nvim_lsp",
+            diagnostics_indicator = function(count)
+				return "(" .. count .. ")"
+			end,
 			},
 		},
 		keys = {
