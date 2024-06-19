d7e7e38820d9e66ac7abcbad54a7f815be42340e
Nhanjk <tranchinhan20102021@gmail.com>
2024-06-19 10:27:02 +0700

    feat(core): add new keymap for lsp, move lsp keymap to keymap.lua

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -14,8 +14,12 @@ vim.keymap.set("n", "<C-Up>", "<cmd>resize -2<CR>", { desc = "Resize up" })
 vim.keymap.set("n", "<C-Down>", "<cmd>resize +2<CR>", { desc = "Resize down" })
 vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<CR>", { desc = "Resize left" })
 vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<CR>", { desc = "Resize right" })
-vim.keymap.set("n", "<S-Down>", function() vim.api.nvim_feedkeys("5j", "n", true) end, { desc = "Fast move down" })
-vim.keymap.set("n", "<S-Up>", function() vim.api.nvim_feedkeys("5k", "n", true) end, { desc = "Fast move up" })
+vim.keymap.set("n", "<S-Down>", function()
+    vim.api.nvim_feedkeys("5j", "n", true)
+end, { desc = "Fast move down" })
+vim.keymap.set("n", "<S-Up>", function()
+    vim.api.nvim_feedkeys("5k", "n", true)
+end, { desc = "Fast move up" })
 vim.keymap.set("n", "<C-s>", "<cmd>w<CR>", { desc = "Save file" })
 vim.keymap.set("n", "<leader>|", "<cmd>vsplit<cr>", { desc = "Vertical Split" })
 vim.keymap.set("n", "<leader>\\", "<cmd>split<cr>", { desc = "Horizontal Split" })
@@ -30,5 +34,40 @@ vim.keymap.set("t", "<C-h>", "<C-\\><C-N><C-w>h", { desc = "Terminal left window
 vim.keymap.set("t", "<C-j>", "<C-\\><C-N><C-w>j", { desc = "Terminal down window navigation" })
 vim.keymap.set("t", "<C-k>", "<C-\\><C-N><C-w>k", { desc = "Terminal up window navigation" })
 vim.keymap.set("t", "<C-l>", "<C-\\><C-N><C-w>l", { desc = "Terminal right window navigation" })
+
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(ev)
+        vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+        local icons = {
+            misc = require("core").gets("misc"),
+        }
+
+
+        local function map(mode, key, core, desc)
+            vim.keymap.set(mode, key, core, { desc = desc, buffer = ev.buf })
+        end
+
+        -- Using the custom map function
+
+        map("n", "<leader>l", "<leader>l", icons.misc.LSP .. "LSP")
+
+        map("n", "<leader>lh", vim.lsp.buf.signature_help, "Show signature help")
+
+        map('n', 'ld', vim.lsp.buf.definition, "Show definition")
+
+        map("n", "<leader>li", "<cmd>LspInfo<cr>", "Lsp Info")
+
+        map("n", "<leader>lk", vim.lsp.buf.hover, "Hover")
+
+        map("n", "[d", vim.diagnostic.goto_prev, "Previous Diagnostic")
+
+        map("n", "]d", vim.diagnostic.goto_next, "Next Diagnostic")
+
+        map("n", "<leader>ln", vim.lsp.buf.rename, "Rename Varable")
+
+        map({ "n", "v" }, "<leader>lf", vim.lsp.buf.format, "Format buffer")
+    end,
+})
 
 require("custom.keymaps")
