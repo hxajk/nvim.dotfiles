398440baac520cf6a569434dc11942d10734fef8
Chi nhan <hxajkzzz@gmail.com>
2024-06-27 08:35:55 +0700

    fix(tools.lua): Fix gitsign warning on linux

--- lua/plugins/1-tools.lua
+++ lua/plugins/1-tools.lua
@@ -102,38 +102,7 @@ local default = {
         enabled = vim.fn.executable("git") == 1,
 
         opts = {
-            signs = {
-                add = {
-                    hl = "GitSignsAdd",
-                    text = "│",
-                    numhl = "GitSignsAddNr",
-                    linehl = "GitSignsAddLn",
-                },
-                change = {
-                    hl = "GitSignsChange",
-                    text = "│",
-                    numhl = "GitSignsChangeNr",
-                    linehl = "GitSignsChangeLn",
-                },
-                delete = {
-                    hl = "GitSignsDelete",
-                    text = "_",
-                    numhl = "GitSignsDeleteNr",
-                    linehl = "GitSignsDeleteLn",
-                },
-                topdelete = {
-                    hl = "GitSignsDelete",
-                    text = "‾",
-                    numhl = "GitSignsDeleteNr",
-                    linehl = "GitSignsDeleteLn",
-                },
-                changedelete = {
-                    hl = "GitSignsChange",
-                    text = "~",
-                    numhl = "GitSignsChangeNr",
-                    linehl = "GitSignsChangeLn",
-                },
-            },
+
             watch_gitdir = { interval = 1000, follow_files = true },
             current_line_blame = true,
             current_line_blame_opts = { delay = 1000, virtual_text_pos = "eol" },
